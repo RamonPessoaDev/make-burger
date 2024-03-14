ab1dae3faffec0bf59189dce83171ce9e182df65
Ramon Pessoa <ramonpessoad@hotmail.com>
2024-03-14 12:42:31 -0300

    Change url api for backend

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -54,7 +54,7 @@
 <script>
 import Message from "./Message.vue";
 
-const URL = "http://localhost:3000";
+const URL = "https://makeburger-api.onrender.com";
 
 export default {
   name: "Dashboard",
